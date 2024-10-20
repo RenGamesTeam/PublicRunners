83dff043ca611921e400fbc88fbbf7c4e3f10e89
Shaun Fong <shaunfong@qq.com>
2024-10-21 07:57:19 +0800

    Update setup.sh

--- src/ubuntu/setup.sh
+++ src/ubuntu/setup.sh
@@ -4,10 +4,10 @@
 mkdir actions-runner && cd actions-runner
 
 # Download the latest runner package
-curl -o actions-runner-linux-x64-2.319.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.319.1/actions-runner-linux-x64-2.319.1.tar.gz
+curl -o actions-runner-linux-x64-2.320.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.320.0/actions-runner-linux-x64-2.320.0.tar.gz
 
 # Optional: Validate the hash
-echo "3f6efb7488a183e291fc2c62876e14c9ee732864173734facc85a1bfb1744464  actions-runner-linux-x64-2.319.1.tar.gz" | shasum -a 256 -c
+echo "93ac1b7ce743ee85b5d386f5c1787385ef07b3d7c728ff66ce0d3813d5f46900  actions-runner-linux-x64-2.320.0.tar.gz" | shasum -a 256 -c
 
 # Extract the installer
-tar xzf ./actions-runner-linux-x64-2.319.1.tar.gz
+tar xzf ./actions-runner-linux-x64-2.320.0.tar.gz
